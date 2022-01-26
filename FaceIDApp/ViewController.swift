e588e90dc024098a18497ecf97c89d665a0aaacf
Marco Loprete <marco.loprete@loprete.ch>
2022-01-26 18:07:57 +0100

    First Commit

--- FaceIDApp/ViewController.swift
+++ FaceIDApp/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        let authConext = LAContext()
+        var error : NSError?
+        
+        if authConext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authConext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { success, error in
+                if success == true {
+                    //Success
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                    
+                } else {
+                    DispatchQueue.main.async {
+                        self.signInLabel.text = "Error"
+                    }
+                   
+                }
+            }
+            
+        } else {
+            showAlert(titleInput: "Error", messageInput: "No Biometric Authentication supported by this device")
+        }
+    }
+    
+    func showAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) 
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
